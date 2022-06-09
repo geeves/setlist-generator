094e2918078255f40e3d49921f3b05dbc4acb9ad
geeves <geeves@gmail.com>
2022-06-08 21:31:52 -0400

    feat(reduxtoolkit): Update installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 rm -rf ./.data
+rm -rf ./src/asset/data
 mkdir ./.data
+mkdir ./src/asset/data
 
 ./scripts/buildTracksFromLexicon.js
 
 sleep 5
+
+mv ./.data/alltracks.json ./src/asset/data/alltracks.json
 
 # clean up temporary files
 rm ./.data/tracks*.json
